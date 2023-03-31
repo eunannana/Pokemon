e9d8cac5953702057fd0e887a2e297b692b444b7
Annisa Divayu Andriyani <95303337+eunannana@users.noreply.github.com>
2023-04-01 04:04:14 +0700

    create constants for colors, image, size, and text

---
+++ lib/src/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const tWhiteColor = Colors.white;
+const tRedColor = Color.fromARGB(255, 197, 71, 71);
+const tYellowColor = Color.fromARGB(255, 230, 209, 13);
+const tBlueColor = Color.fromARGB(255, 45, 92, 131);
+const tDarkColor = Colors.black;
+const tCardBgColor = Color(0xFFF7F6F1);
+
---
+++ lib/src/constants/image_string.dart
@@ -0,0 +1,2 @@
+const String tWelcomeImage = "assets/images/pokeball.png";
+const String tNext = "assets/images/next.png";
